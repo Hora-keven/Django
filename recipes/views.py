351ac01174c500eda7dfc4392cff62bee6b2e1a6
hora-keven <kevensantos993@gmail.com>
2023-08-03 11:31:02 -0300

    up

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def contato(request):
-    return render(request, 'home.html', context={
+    return render(request, './pages/home.html', context={
         'name':'Saborosas'})
