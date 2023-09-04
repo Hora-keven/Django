8391ebeaba93e3a5f753998af749dfc2268e058f
hora-keven <>
2023-09-04 15:35:26 -0300

    updt

--- recipes/views.py
+++ recipes/views.py
@@ -3,15 +3,17 @@ from urls.recipes.factory import make_recipe
 from .models import Recipe
 
 def contato(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all( ).order_by('-id')
     return render(request, 'home.html', context={'recipes':recipes})
 
 
 def recipe(request, id):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all()
     return render(request, 'recipe_view.html', context={'recipes':recipes, 'is_detail_page':True})
 
 
 def category(request, category_id):
-    category = Recipe.objects.filter(category__id = category_id)
+    category = Recipe.objects.filter(
+        is_published=True,
+        category__id = category_id)
     return render(request, 'home.html', context={'category':category})
