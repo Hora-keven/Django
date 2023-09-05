a6093750b2282f662ca3dfe9d425c7a670782c3a
hora-keven <kevensantos993@gmail.com>
2023-09-05 14:33:13 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from urls.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404
 from .models import Recipe
-from django.http import HttpResponse, Http404
+
 
 def contato(request):
     recipes = Recipe.objects.all( ).order_by('-id')
@@ -9,24 +8,20 @@ def contato(request):
 
 
 def recipe(request, id):
-    recipes = Recipe.objects.filter(id__icontains = id)
+    recipes = Recipe.objects.filter(pk = id)
     return render(request, 'recipe_view.html', context={'recipe':recipes, 'is_detail_page':True})
 
 
 def category(request, category_id):
-    recipe = Recipe.objects.filter(category__id = category_id, is_published=True)
-    if not recipe:
-        #return HttpResponse(content='Not found', status=404)
-        raise Http404('Not found, please verify the url ')
-    return render(request, 'category.html', context={'category':recipe, 'title':f'{recipe.first().category.name}'})
+    # recipe = Recipe.objects.filter(category__id = category_id, is_published=True)
+    # if not recipe:
+    #     #return HttpResponse(content='Not found', status=404)
+    #     raise Http404('Not found, please verify the url ')
+    recipe = get_list_or_404(
+        Recipe.objects.filter(
+        category_id=category_id,
+        is_published=True
+        ).order_by('-id'))
+    return render(request, 'category.html', context={'category':recipe})
 
-    recipes = Recipe.objects.all()
-    return render(request, 'recipe_view.html', context={'recipes':recipes, 'is_detail_page':True})
-
-
-def category(request, category_id):
-    category = Recipe.objects.filter(
-        is_published=True,
-        category__id = category_id)
-    return render(request, 'home.html', context={'category':category})
 
